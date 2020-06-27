6c037b614f094bf1169e9cb03c552c7a6a371efd
Bogdan <bogdan7morozyuk@gmail.com>
2020-06-27 12:04:01 +0300

    feat: create a repository list item

---
+++ src/components/RepositoryListItem.js
@@ -0,0 +1,68 @@
+import React from "react";
+import StarIcon from '@material-ui/icons/Star';
+import PeopleIcon from '@material-ui/icons/People';
+import { Typography, Chip, makeStyles } from "@material-ui/core";
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: '1rem'
+  },
+  summaryContainer: {
+    flexDirection: 'column'
+  },
+  summaryHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: '1rem'
+  },
+  chip: {
+    marginLeft: '0.5rem'
+  }
+});
+
+const RepositoryListItem = ({ repo, expanded, onToggled }) => {
+  const classes = useStyles();
+  
+  const {
+    node: {
+      name,
+      descriptionHTML,
+      owner: {
+        login
+      },
+      stargazers: {
+        totalCount: totalStarCount
+      }
+    }
+  } = repo;
+
+  return (
+    <ExpansionPanel
+      expanded={expanded}
+      onChange={onToggled}
+      className={classes.root}
+    >
+      <ExpansionPanelSummary classes={{content: classes.summaryContainer}}>
+        <div className={classes.summaryHeader}>
+          <Typography variant="h6">{name}</Typography>
+          <div>
+            <Chip label={`by ${login}`} avatar={<PeopleIcon />} className={classes.chip} />
+            <Chip label={totalStarCount} avatar={<StarIcon />} className={classes.chip} />
+          </div>
+        </div>
+        <Typography 
+          variant="caption"
+          dangerouslySetInnerHTML={{__html: descriptionHTML}}
+          component="div"
+        />
+      </ExpansionPanelSummary>
+      <ExpansionPanelDetails>
+        Issues here!
+      </ExpansionPanelDetails>
+    </ExpansionPanel>
+  );
+};
+
+export default RepositoryListItem;
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as SearchBar } from "src/components/SearchBar";
 export { default as RepositoryList } from "src/components/RepositoryList";
+export { default as RepositoryListItem } from "src/components/RepositoryListItem";
